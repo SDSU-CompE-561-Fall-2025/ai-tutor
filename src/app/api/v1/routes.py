045a108f5f10a4ae2cbcaa8c7deb73f4935fd87f
Dariel <darielgutierrez332@gmail.com>
2025-10-28 16:25:47 -0700

    fixed stash conflict

--- src/app/api/v1/routes.py
+++ src/app/api/v1/routes.py
@@ -7,7 +7,6 @@ from app.routes import (
     google_drive,
     tutor_session,
     user,
-    video_generation,
 )
 
 api_router = APIRouter(prefix="/api/v1")
@@ -19,4 +18,3 @@ api_router.include_router(course.api_router)
 api_router.include_router(google_drive.api_router)
 api_router.include_router(tutor_session.api_router)
 api_router.include_router(chat_message.api_router)
-api_router.include_router(video_generation.api_router)
