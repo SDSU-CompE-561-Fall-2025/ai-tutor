32c7f46c336d64b64dba852430b104d3009bbb47
JacobTang06 <jake.tang@gmail.com>
2025-12-12 21:42:45 -0800

    remove axios api

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,10 +1,4 @@
-import axios from "axios";
-
-const API_URL = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
-
-export default API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 type Course = {
   id: number;
